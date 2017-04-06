52133f928e883fdafe975b9050f6dda9e2d1a68e
Vaclav Pavlicek <vaclav@pavlicek.cz>
2017-04-06 23:08:19 +0200

    add diagonal gradient controller

--- src/js/services/linearGradients.service.js
+++ src/js/services/linearGradients.service.js
@@ -34,6 +34,23 @@
                 "background": "linear-gradient(to right, " + "rgb(" + firstColorRGB.red + ", " + firstColorRGB.green + ", " + firstColorRGB.blue + "),rgb("
                 + secondColorRGB.red + ", " + secondColorRGB.green + ", " + secondColorRGB.blue + ")"
             };
-        }
+        };
+
+        this.createDiagonalGradient = function (firstColorRGB, secondColorRGB) {
+            return {
+                "background": "rgb(" + firstColorRGB.red + ", " + firstColorRGB.green + ", " + firstColorRGB.blue + ")",
+                "background": "-webkit-linear-gradient(left top, "
+                + "rgb(" + firstColorRGB.red + ", " + firstColorRGB.green + ", " + firstColorRGB.blue + "),rgb("
+                + secondColorRGB.red + ", " + secondColorRGB.green + ", " + secondColorRGB.blue + ")",
+                "background": "-o-linear-gradient(bottom right, "
+                + "rgb(" + firstColorRGB.red + ", " + firstColorRGB.green + ", " + firstColorRGB.blue + "),rgb("
+                + secondColorRGB.red + ", " + secondColorRGB.green + ", " + secondColorRGB.blue + ")",
+                "background": "-moz-linear-gradient(bottom right, "
+                + "rgb(" + firstColorRGB.red + ", " + firstColorRGB.green + ", " + firstColorRGB.blue + "),rgb("
+                + secondColorRGB.red + ", " + secondColorRGB.green + ", " + secondColorRGB.blue + ")",
+                "background": "linear-gradient(to bottom right, " + "rgb(" + firstColorRGB.red + ", " + firstColorRGB.green + ", " + firstColorRGB.blue + "),rgb("
+                + secondColorRGB.red + ", " + secondColorRGB.green + ", " + secondColorRGB.blue + ")"
+            };
+        };
     });
 })();
